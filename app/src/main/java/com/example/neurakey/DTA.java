8b5fd067837efc52f3a3d1da51a26f96d1df6082
samjoseuoft <sam.jose@mail.utoronto.ca>
2023-02-03 10:20:12 -0500

    Update (Feb 3, 2023 @ 10:19AM)

--- app/src/main/java/com/example/neurakey/DTA.java
+++ app/src/main/java/com/example/neurakey/DTA.java
@@ -3,12 +3,21 @@ package com.example.neurakey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DTA extends AppCompatActivity {
+
+    private TextView txtDTAPromptQ;
+    private EditText etxtDTAPromptA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dta);
+
+        etxtDTAPromptA = findViewById(R.id.etxtDTAPromptA);
+        txtDTAPromptQ = findViewById(R.id.txtDTAPromptQ);
+
     }
 }
