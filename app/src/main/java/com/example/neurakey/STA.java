b4af2a539d0ccb1c7d1fd01a9c62f75f1e41406a
samjoseuoft <sam.jose@mail.utoronto.ca>
2023-02-03 10:09:17 -0500

    Update (Feb 3, 2023 @ 10:08AM)

--- app/src/main/java/com/example/neurakey/STA.java
+++ app/src/main/java/com/example/neurakey/STA.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class STA extends AppCompatActivity {
-    private TextView txtSTAPromptA;
+
     private TextView txtSTAPromptQ;
+    private EditText etxtSTAPromptA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sta);
 
-        txtSTAPromptA = findViewById(R.id.txtSTAPromptA);
+        etxtSTAPromptA = findViewById(R.id.etxtSTAPromptA);
         txtSTAPromptQ = findViewById(R.id.txtSTAPromptQ);
 
     }
